586ef47434b50d2463aead92c8f35cc8ff6368b7
luke-teo <67887422+teoyi@users.noreply.github.com>
2024-09-19 19:23:47 +0900

    fix: indent size, added typescript lsp to auto enroll

--- lua/options.lua
+++ lua/options.lua
@@ -79,8 +79,8 @@ vim.opt.incsearch = true
 vim.opt.smarttab = true
 
 -- Set tab and indentation size
-vim.opt.tabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 
 -- Enable hidden buffers
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -92,6 +92,7 @@ return {
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
 
       local servers = {
+        vtsls = {},
         gopls = {},
         pyright = {
           settings = {
