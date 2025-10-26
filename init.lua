a47aebad0361b489c60fe12c35a646c55d2c8096
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-10-26 22:18:24 +0900

    feat: chagned to light mode lunaperche

--- init.lua
+++ init.lua
@@ -7,7 +7,7 @@ require 'statusline'
 require 'keymaps'
 require 'bootstrap'
 require 'plugins'
--- vim.o.background = 'light'
--- vim.cmd.colorscheme 'lunaperche'
+vim.o.background = 'light'
+vim.cmd.colorscheme 'lunaperche'
 
 -- vim: ts=2 sts=2 sw=2 et
