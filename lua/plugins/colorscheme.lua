e580f1ab8bec583451f811e2bf7265e526283aee
luke-teo <67887422+lukteo@users.noreply.github.com>
2025-12-20 23:25:21 +0900

    feat: updated to use tempus theme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,15 +1,24 @@
 -- Colorscheme used
 return {
+  -- {
+  --   'ribru17/bamboo.nvim',
+  --   lazy = false,
+  --   priority = 1000,
+  --   config = function()
+  --     require('bamboo').setup {
+  --       transparent = true,
+  --       -- optional configuration here
+  --     }
+  --     require('bamboo').load()
+  --   end,
+  -- },
   {
-    'ribru17/bamboo.nvim',
-    lazy = false,
-    priority = 1000,
+    'protesilaos/tempus-themes-vim',
+    name = 'tempus_classic', -- You can name it anything, but this is descriptive
+    priority = 1000, -- ensures the colorscheme loads early
     config = function()
-      require('bamboo').setup {
-        transparent = true,
-        -- optional configuration here
-      }
-      require('bamboo').load()
+      -- Set the colorscheme after the plugin is loaded
+      vim.cmd.colorscheme 'tempus_classic'
     end,
   },
 }
